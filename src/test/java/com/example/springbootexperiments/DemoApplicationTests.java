e1a5d698ca57006e991c44c9a6eeacccd77e6a3f
Kaustubh <kkaustubh@us.ibm.com>
2019-11-24 11:23:46 -0800

    Updated dependency for testing with spring-test, junit and mockito. Removed `spring-boot-starter-test` from pom.xml

--- src/test/java/com/example/springbootexperiments/DemoApplicationTests.java
+++ src/test/java/com/example/springbootexperiments/DemoApplicationTests.java
@@ -2,11 +2,9 @@ package com.example.springbootexperiments;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class DemoApplicationTests {
 
 	@Test
