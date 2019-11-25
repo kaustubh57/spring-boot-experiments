60947d0e1ee6aaf589581f982bad48272912b778
Kaustubh <kkaustubh@us.ibm.com>
2019-11-25 09:21:22 -0800

    Added spring boot aop and asjectj dependency.

--- src/main/java/com/example/springbootexperiments/DemoApplication.java
+++ src/main/java/com/example/springbootexperiments/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.springbootexperiments;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -7,8 +9,15 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class DemoApplication {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
+
     public static void main(String[] args) {
         // ###### RUN ######
         ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
+
+        //  Check various spring beans loaded in application context
+//        for (String name : applicationContext.getBeanDefinitionNames()) {
+//            LOGGER.info("#### Bean: {}", name);
+//        }
     }
 }
